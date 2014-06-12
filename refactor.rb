e327da2ad2475e9f723fa858a36c6c523037dce6
Aidan Feldman <afeld@github.com>
2014-06-12 02:17:11 -0400

    show possible matches within the file

--- refactor.rb
+++ refactor.rb
@@ -6,6 +6,7 @@ to = ARGV[1] || raise("Needs a TO argument.")
 
 from_camelized = from.camelize
 from_dashed = from.dasherize
+from_humanized = from.humanize
 from_underscored = from.underscore
 to_camelized = to.camelize
 to_dashed = to.dasherize
@@ -13,15 +14,16 @@ to_underscored = to.underscore
 
 camelized_regex = /(?<=\b|_)#{Regexp.quote(from_camelized)}(?=\b|_)/
 dashed_regex = /(?<=\b|_)#{Regexp.quote(from_dashed)}(?=\b|_)/
+humanized_regex = /(?<=\b|_)#{Regexp.quote(from_humanized)}(?=\b|_)/i
 underscored_regex = /(?<=\b|_)#{Regexp.quote(from_underscored)}(?=\b|_)/
 
 # all files in current directory
 Dir.glob('**/*') do |old_path|
   # ignore certain directories
   unless old_path =~ %r{\A(coverage|pkg|tmp|vendor)(\z|/)}
-    puts old_path
-
-    if File.file?(old_path)
+    is_file = File.file?(old_path)
+    if is_file
+      # replace within file
       old_text = File.read(old_path)
 
       new_text = old_text.dup
@@ -41,11 +43,25 @@ Dir.glob('**/*') do |old_path|
     new_basename.gsub!(dashed_regex, to_dashed)
     new_basename.gsub!(underscored_regex, to_underscored)
 
-    unless new_basename == old_basename
+    if new_basename == old_basename
+      # no change
+      new_path = old_path
+    else
       # rename file
       new_path = File.join(File.dirname(old_path), new_basename)
       puts "#{old_path} –> #{new_path}"
       FileUtils.mv(old_path, new_path)
+    end
+
+    if is_file
+      # show possible matches in body
+      line_num = 0
+      new_text.each_line do |line|
+        if line =~ humanized_regex
+          puts "#{new_path}:#{line_num}: #{line.strip}"
+        end
+        line_num += 1
+      end
     end
   end
 end
